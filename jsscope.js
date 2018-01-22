775b4a76f6b4269bc2f4694483e5bcd903bcde49
Unknown <1780089G@student.tp.edu.sg>
2018-01-22 15:42:21 +0800

    22 jan 4pm

--- jsscope.js
+++ jsscope.js
@@ -6,17 +6,24 @@ while(input !== "quit"){
     if(input === "list") {
       console.log("*******")
       todos.forEach(function(todo, i){
-        console.log(i + ": " + todo);
-        console.log("*******")
+        console.log(i + ": " + todo); 
       });
-
+      console.log("*******")
   } else if(input === "new") {
   //ask for new tod do
   //add to the todos array
     var newTodo = prompt("Enter new todo");
   //add to dos array
     todos.push(newTodo);
-  }
+
+  } else if(input === "delete"){
+      // ask for index of to do to be delete
+      var index = prompt("Enter index of todo to delete");
+      // delete that to do
+      //splice
+      todos.splice(index,1);
+    }
+  
   input = prompt("What would you like to do?");
 }
 console.log("ok, you quit the app");
