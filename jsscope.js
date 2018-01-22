cbef5f8e9d9cb8fa9b1c2194295c5342b4d1ab69
Unknown <1780089G@student.tp.edu.sg>
2018-01-22 16:00:23 +0800

    22 jan 4pm1

--- jsscope.js
+++ jsscope.js
@@ -4,26 +4,37 @@ var input = prompt("What would you like to do")
 while(input !== "quit"){
   // handle input
     if(input === "list") {
-      console.log("*******")
-      todos.forEach(function(todo, i){
-        console.log(i + ": " + todo); 
-      });
-      console.log("*******")
+      listTodos();
   } else if(input === "new") {
-  //ask for new tod do
-  //add to the todos array
-    var newTodo = prompt("Enter new todo");
-  //add to dos array
-    todos.push(newTodo);
+      addTodo();
 
   } else if(input === "delete"){
-      // ask for index of to do to be delete
-      var index = prompt("Enter index of todo to delete");
-      // delete that to do
-      //splice
-      todos.splice(index,1);
+      deleTodo();
     }
   
   input = prompt("What would you like to do?");
 }
 console.log("ok, you quit the app");
+function listTodos(){
+      console.log("*******")
+      todos.forEach(function(todo, i){
+        console.log(i + ": " + todo); 
+      });
+      console.log("*******")
+    }
+function addTodo(){
+  //ask for new tod do
+  //add to the todos array
+  var newTodo = prompt("Enter new todo");
+  //add to dos array
+    todos.push(newTodo);
+  console.log("Added todo");
+}
+function deleteTodo(){
+  // ask for index of to do to be delete
+  var index = prompt("Enter index of todo to delete");
+  // delete that to do
+  //splice
+    todos.splice(index,1);
+  console.log("Deleted todo");
+}
